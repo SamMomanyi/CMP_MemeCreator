f13c6b2224940c5b9e9bdfe1174b295c5f673b4a
Samson Momanyi <samsonmomanyi2005@gmail.com>
2026-01-23 10:51:21 +0300

    Initial Commit

--- composeApp/src/commonMain/kotlin/com/plcoding/cmp_memecreator/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/cmp_memecreator/App.kt
@@ -1,14 +1,15 @@
 package com.plcoding.cmp_memecreator
 
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.plcoding.cmp_memecreator.core.presentation.NavigationRoot
-import com.plcoding.cmp_memecreator.core.theme.MemeCreatorTheme
+
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MemeCreatorTheme {
-        NavigationRoot()
+    MaterialTheme{
+        Text("Hello World")
     }
 }
